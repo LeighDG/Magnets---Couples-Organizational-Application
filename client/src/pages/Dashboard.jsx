1eb7ce90f64e401b51be0bc3ab2881a02bc8f073
Leigh Graham <148042871+LeighDG@users.noreply.github.com>
2026-01-15 11:02:22 +0200

    feat: Dashboard layout created

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import BackgroundLayout from "../components/BackgroundLayout";
+import LayoutWrapper from "../components/Layout-Wrapper";
+
+export default function DashboardPage() {
+  return (
+    <BackgroundLayout>
+      <LayoutWrapper>
+        {/* Dashboard content goes here */}
+      </LayoutWrapper>
+    </BackgroundLayout>
+  );
+}
